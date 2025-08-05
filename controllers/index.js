863aeca558fd78f1726f9184eb8c1290a5e832d6
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2025-08-05 17:19:38 +0530

    refactor(controller): centralize all controllers in index.js for cleaner imports

---
+++ controllers/index.js
@@ -0,0 +1,56 @@
+const { goalController } = require("./goalDetailController");
+const {
+  newGoalFormControllerGet,
+  newGoalFormControllerPost,
+  editGoalFormControllerGet,
+  editGoalFormControllerPost,
+  goalDelete,
+} = require("./goalFormController");
+const { goalsController } = require("./goalsListController");
+const { indexController } = require("./indexController");
+const { profileController } = require("./profileController");
+const { tagController } = require("./tagDetailController");
+const {
+  newTagFormControllerGet,
+  newTagFormControllerPost,
+  editTagFormControllerGet,
+  editTagFormControllerPost,
+  tagDelete,
+} = require("./tagFormController");
+const { tagsController } = require("./tagsListController");
+const {
+  newTaskFormControllerGet,
+  newTaskFormControllerPost,
+  editTaskFormControllerGet,
+  editTaskFormControllerPost,
+  taskDelete,
+} = require("./taskFormController");
+const { tasksController, taskToggle } = require("./tasksController");
+
+const controllers = {
+  indexController,
+  profileController,
+  goalController,
+  goalsController,
+  newGoalFormControllerGet,
+  newGoalFormControllerPost,
+  editGoalFormControllerGet,
+  editGoalFormControllerPost,
+  goalDelete,
+  tagController,
+  tagsController,
+  newTagFormControllerGet,
+  newTagFormControllerPost,
+  editTagFormControllerGet,
+  editTagFormControllerPost,
+  tagDelete,
+  tasksController,
+  taskToggle,
+  newTaskFormControllerGet,
+  newTaskFormControllerPost,
+  editTaskFormControllerGet,
+  editTaskFormControllerPost,
+  taskDelete,
+};
+
+module.exports = { controllers };
