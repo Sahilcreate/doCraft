b3063eab0dc900bbf99ce37292185d259292f08b
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2025-08-05 17:00:57 +0530

    refactor: clean up app.js and integrate route modules via centralized route object

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
+// Load environment variables
 require("dotenv").config();
 
-const express = require("express");
-const app = express();
-
+// Core modules
 const path = require("node:path");
 
+// Third party modules
+const express = require("express");
+
+// App initialization
+const app = express();
+
+// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
-
+const { setCurrentUser } = require("./middlewares/setCurrentUser");
 app.use(setCurrentUser);
-app.use("/");
-app.use("/profile");
-app.use("/goals");
-app.use("/tags");
-app.use("/tasks");
 
+// Routes
+const { routes } = require("./routes/index");
+app.use("/", routes.indexRouter);
+app.use("/profile", routes.profileRouter);
+app.use("/goals", routes.goalsRouter);
+app.use("/tags", routes.tagsRouter);
+app.use("/tasks", routes.tasksRouter);
+
+// Error handling middleware
 app.use((err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
+  res.status(500).send("Something broke!");
 });
 
+// Server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`doCraft - listening to port ${PORT}`);
