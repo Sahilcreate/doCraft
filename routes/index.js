7759957d533c6e163aa9856b396051f2c3828436
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2025-08-05 17:42:56 +0530

    refactor(router): centralize router imports and exports

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const { goalsRouter } = require("./goalsRouter");
+const { indexRouter } = require("./indexRouter");
+const { profileRouter } = require("./profileRouter");
+const { tagsRouter } = require("./tagsRouter");
+const { tasksRouter } = require("./tasksRouter");
+
+const routes = {
+  indexRouter,
+  profileRouter,
+  tagsRouter,
+  goalsRouter,
+  tasksRouter,
+};
+
+module.exports = { routes };
