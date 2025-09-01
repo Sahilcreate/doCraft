57d5d3bd65df01ac76e2548b9ad34a1f751dc80e
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2025-09-01 12:21:48 +0530

    feat(core): add edit profile functionality

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,8 +1,19 @@
 const { Router } = require("express");
 const { controllers } = require("../controllers/index");
+const { ensureRole } = require("../middlewares/auth");
 
 const profileRouter = Router();
 
 profileRouter.get("/", controllers.profileController);
+profileRouter.get(
+  "/edit",
+  ensureRole(["user", "admin"]),
+  controllers.editProfileGet
+);
+profileRouter.post(
+  "/edit",
+  ensureRole(["user", "admin"]),
+  controllers.editProfilePost
+);
 
 module.exports = { profileRouter };
